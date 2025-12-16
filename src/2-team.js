bc1c6d00fb20eb0a0c2ffdb113359e7f1ab22070
Syed <xayanmay@gmail.com>
2025-12-16 13:04:17 -0500

    Worked on 2-team

--- src/2-team.js
+++ src/2-team.js
@@ -2,11 +2,36 @@
 // Implement your Player and Team classes below
 
 class Player {
-
+    constructor(name, position, jerseyNumber){
+        this.name = name
+        this.position = position
+        this.jerseyNumber = jerseyNumber
+    }
 }
 
 class Team {
+    #starters = []
+    #bench = []
+    
+    constructor(name){
+        this.name = name
+    }
+    get getStarter(){
+        return this.#starters
+    }
 
+    get getBench(){
+        return this.#bench
+    }
+    addBenchPlayer(player){
+        this.#bench.push(player)
+    }
+    getPlayerCount(){
+        return this.#starters.length + this.#bench.length
+    }
+    moveToBench(name){
+        
+    }
 }
 
 module.exports = { Player, Team };
