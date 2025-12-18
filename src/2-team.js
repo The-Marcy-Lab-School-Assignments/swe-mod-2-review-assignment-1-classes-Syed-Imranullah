31d65665b99a1fe2fedd40441d0928abf27b3e60
Syed <xayanmay@gmail.com>
2025-12-18 12:08:40 -0500

    updated

--- src/2-team.js
+++ src/2-team.js
@@ -2,37 +2,53 @@
 // Implement your Player and Team classes below
 
 class Player {
-    constructor(name, position, jerseyNumber){
-        this.name = name
-        this.position = position
-        this.jerseyNumber = jerseyNumber
-    }
+  constructor(name, position, jerseyNumber) {
+    this.name = name;
+    this.position = position;
+    this.jerseyNumber = jerseyNumber;
+  }
 }
 
 class Team {
-    #starters = []
-    #bench = []
-    
-    constructor(name){
-        this.name = name
-    }
-    get getStarter(){
-        return this.#starters
-    }
+  #starters = [];
+  #bench = [];
 
-    get getBench(){
-        return this.#bench
+  constructor(name) {
+    this.name = name;
+  }
+  get getStarters() {
+    return this.#starters;
+  }
+
+  get getBench() {
+    return this.#bench;
+  }
+  addBenchPlayer(player) {
+    this.#bench.push(player);
+  }
+  getPlayerCount() {
+    return this.#starters.length + this.#bench.length;
+  }
+  moveToBench(name) {
+    for (let i = 0; i < this.#starters.length; i++) {
+      if (this.#starters[i].name === name) {
+        const player = this.#starters.splice(name);
+        this.#bench.push(Player);
+        return true;
+      }
     }
-    addBenchPlayer(player){
-        this.#bench.push(player)
+    return false;
+  }
+  moveToStarters(name){
+    for (let i = 0; i < this.#bench.length; i ++){
+      if(this.#bench[i].name === name){
+        const benchPlayer = this.#bench.splice(name)
+          this.#bench.push(Player)
+          return true
+      }
     }
-    getPlayerCount(){
-        return this.#starters.length + this.#bench.length
-    }
-    moveToBench(name){
-        
-    }
+    return false
+  }
 }
 
 module.exports = { Player, Team };
-
